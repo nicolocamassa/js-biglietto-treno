e85d19110a807c6542db69ad5b42ab2765d039d1
nicolocamassa <camassanicolo03@gmail.com>
2023-11-20 14:49:06 +0100

    [UPDATE] added age check and discount calculation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
+/* Input per conoscere il tipo di viaggio ed il viaggiatore */
 let distanceKm = prompt('Quanti chilometri vuoi percorrere? ');
 let age = prompt('Quanti anni hai?');
-const priceKm = 0.21;
 
+/* Calcolo del prezzo del biglietto */
+const priceKm = 0.21;
 ticketPrice = distanceKm * priceKm;
 
-if(age < 18){
-    
-}else if(age > 65){
+/* Gestione degli sconti */
+let discountUnder = 20;
+let discountOver = 40;
 
+/* Controllo dell'età del passeggero */
+if(age < 18){
+    ticketPrice -= ticketPrice * discountUnder / 100;
+    console.log(ticketPrice);
+}else if(age > 65){
+    ticketPrice -= ticketPrice * discountOver / 100;
+    console.log(ticketPrice);
+}else{
+    console.log(ticketPrice);
 }
