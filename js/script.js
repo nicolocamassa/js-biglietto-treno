a99a1f409e1b8d5abffa09d401872046ca81df80
nicolocamassa <camassanicolo03@gmail.com>
2023-11-20 14:42:00 +0100

    [UPDATE] calculation of the ticket price

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let distanceKm = prompt('Quanti chilometri vuoi percorrere? ');
+let age = prompt('Quanti anni hai?');
+const priceKm = 0.21;
+
+ticketPrice = distanceKm * priceKm;
+
+if(age < 18){
+    
+}else if(age > 65){
+
+}
